794259ad50161df1c99e0151e939da5f4eb459dd
Roni Lindholm <roni.lindholm@gmail.com>
2025-04-14 08:46:51 +0300

    feat: add config for custom templates

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -172,6 +172,7 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             os.path.join(BASE_DIR, "qfieldcloud", "core", "templates"),
+            os.path.join(BASE_DIR, "templates"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
