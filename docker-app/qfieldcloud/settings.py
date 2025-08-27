4e96c24268e93dea880ec948275b8669690a7a13
Joonalai <joona.laine@cofactor.fi>
2025-08-27 15:35:29 +0300

    Add a cron job to reschedule failed apply jobs

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -165,6 +165,7 @@ CRON_CLASSES = [
     "qfieldcloud.core.cron.ResendFailedInvitationsJob",
     "qfieldcloud.core.cron.SetTerminatedWorkersToFinalStatusJob",
     "qfieldcloud.core.cron.DeleteObsoleteProjectPackagesJob",
+    "qfieldcloud.core.cron.RescheduleFailedApplyJobs",
 ]
 
 ROOT_URLCONF = "qfieldcloud.urls"
